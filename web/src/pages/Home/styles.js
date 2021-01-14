6377ba63289c9356c6bf8c6b4f9fa88d314dca0a
viniciusrosa444 <viniciusrosa444@gmail.com>
2021-01-14 13:28:16 -0400

    add carrousel

--- web/src/pages/Home/styles.js
+++ web/src/pages/Home/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import SearchIcon from '../../assets/images/search.svg'
 
 export const Header = styled.header`
-    width: 100%;
+    width: auto;
     height: 60px;
     
     background:#fff;
@@ -49,14 +49,120 @@ export const ImageProfile = styled.img`
 `;
 
 export const Carrinho = styled.a`
-    padding:15px;
+    padding:15px 30px 15px;
 
-    background:red;
+    /* background:red; */
     margin-right:15px;
-
+    margin-bottom:10px;
     svg { 
+        margin : 0;
 
         align-items:center;
         justify-content: center; 
     }
 `
+
+export const CategoryList= styled.main`
+    /* margin-top:25px; */
+    margin: 25px auto;
+    display: flex;
+    width:100%;
+    /* padding: 50px; */
+
+    justify-content:center; //alinha na horizontal
+    flex-direction:row;
+`
+export const CategoryListItem= styled.div`
+    /* margin:15px; */
+    /* padding:10px; */
+    transition: opacity 0.3s;
+    display:block;
+    justify-content:center;
+    text-align:center;
+
+    p {
+       font-weight:bold;
+       font-family: roboto sans-serif;
+    }
+`
+export const Category= styled.img`
+    border-radius:50%;
+    width: 150px;
+    height: 150px;
+    margin:0;
+    transition: opacity 1s;
+    
+    &:hover{
+        cursor: pointer;
+        opacity: 0.6;
+    }
+
+    @media (max-width: 800px) {
+        width: 100px;
+        height: 100px;
+    }
+`;
+
+export const ProductsList = styled.aside`
+    flex:1;
+    flex-wrap: wrap; 
+    margin-top:25px;
+    display: flex;
+    justify-content:center; //alinha na horizontal
+    flex-direction:row;
+`;
+export const ProductListItem = styled.div`
+    width: 230px;
+    height: 320px;
+    margin-right:15px;
+    background:#C4C4C4;
+    border-radius:10px;
+    align-items:center;
+    justify-content:center;
+    transition: opacity 0.8s;
+
+    @media (max-width: 800px) {
+        margin-bottom: 10px;
+        
+        width: 200px;
+        height: 300px;
+    }
+
+    &:hover{
+        width: 240px;
+        cursor: pointer;
+        opacity: 0.6;
+
+        img {
+            width: 220px;
+        }
+        button{
+            width: 220px;
+
+        }
+    }
+`;
+export const Product = styled.img`
+    margin: 8px;
+    width: 210px;
+    height: 236px;
+    border-radius:10px;
+
+`;
+
+export const PayButton = styled.button`
+    background:#f5f8fa;
+    margin: 8px;
+    width: 210px;
+    height: 42px;
+    border-radius:10px;
+    border:none;
+    transition: opacity 0.8s;
+
+    &:hover{
+        /* width: 220px; */
+        cursor: pointer;
+        opacity: #f5f8f6;
+    }
+    
+`;
